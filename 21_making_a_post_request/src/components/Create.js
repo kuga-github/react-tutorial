e0a04a7c9547c0a4486a57914aae81c2a53ed619
kuga-github <kuga.tsunekawa@gmail.com>
2022-07-20 21:58:15 +0900

    first commit

---
+++ 21_making_a_post_request/src/components/Create.js
@@ -0,0 +1,55 @@
+import { TextField,FormGroup, Button } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import {useState} from 'react';
+import axios from 'axios'
+
+const Create = () => {
+    const [title, setTile] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    const [error, setError] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+
+    const handleSubmit = (e) => {
+        setIsPending(true);
+        console.log(isPending);
+        e.preventDefault();
+        axios.post("http://localhost:8000/blogs",
+            {
+                title: title,
+                body: body,
+                author: author
+            }
+        ).catch(error => setError(error))
+        .then(() => {setIsPending(false);
+        console.log(isPending);})
+    }   
+
+    return (
+        <div>
+        <div>{error&&error.message}</div> 
+        <div>
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <FormGroup>
+            <TextField id="outlined-basic" label="タイトル" variant="outlined"
+            onChange={(e) => setTile(e.target.value)} value={title}/>
+            <TextField id="outlined-basic" label="投稿者" variant="outlined"
+            onChange={(e) => setAuthor(e.target.value)} value={author}/>
+            <TextField
+                id="outlined-multiline-static"
+                label="本文"
+                multiline
+                rows={20}
+                onChange={(e) => setBody(e.target.value)}
+                value={body}
+                required
+            />
+            <LoadingButton variant="contained" loading={isPending} type="submit">投稿</LoadingButton>
+            </FormGroup>
+        </form>
+        </div>
+        </div>
+    );
+}
+
+export default Create;
